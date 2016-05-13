822ae1c0b7a28e30c7d237de5ba0ded26f686428
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-05-13 13:56:02 +1000

    Misc

--- proc/post.sh
+++ proc/post.sh
@@ -55,6 +55,19 @@ loadContextFrom(String uuid) {
 replacement=""
 perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
 
+# Notify the current user if they try to add relationships to the current
+# context without having saved it first
+string="
+  newTab(\"Relationship\", true);"
+replacement="
+  String tabgroup = \"Context\";
+  if (isNull(getUuid(tabgroup))){
+    showToast(\"{You_must_save_this_tabgroup_first}\");
+    return;
+  }
+  $string"
+perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
+
 # Code to inherit LOT_ID from Context into Sample
 string="
   parentTabgroup = tabgroup;

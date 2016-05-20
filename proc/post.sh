ec95be06edf012e875fa2c0e9cb0fb1268747cb6
Christian Nassif-Haynes <DoxasticFox@bitbucket.org>
2016-05-20 18:58:05 +1000

    Relationships

--- proc/post.sh
+++ proc/post.sh
@@ -69,6 +69,19 @@ perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
 # Notify the current user if they try to add relationships to the current
 # context without having saved it first
 string="
+  newTab(\"Context_Group_Relationship\", true);"
+replacement="
+  String tabgroup = \"Context_Group\";
+  if (isNull(getUuid(tabgroup))){
+    showToast(\"{You_must_save_this_tabgroup_first}\");
+    return;
+  }
+  $string"
+perl -0777 -i.original -pe "s/\\Q$string/$replacement/igs" ui_logic.bsh
+
+# Notify the current user if they try to add relationships to the current
+# context without having saved it first
+string="
   newTab(\"Relationship\", true);"
 replacement="
   String tabgroup = \"Context\";
